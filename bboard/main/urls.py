86ff722545641c8593397e511b64a01983069aa6
K4sho <lolunoobkaa@yandex.ru>
2020-02-13 21:24:39 +0300

    add: Добавил страницу входа, выхода, начал работу над правками личных данных пользователя

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import index, other_page
+from .views import index, other_page, BBLoginView, BBLogoutView, profile, ChangeUserInfoView
 
 app_name = 'main'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('<str:page>/', other_page, name='other')
+    path('<str:page>/', other_page, name='other'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
 ]
